27c32eb9cbeb264df994215c2ec3bea30f2123d4
Jayjeet1013 <jayjeet1013@gmail.com>
2024-03-17 11:31:55 +0530

    Initial project setup: Next.js installation and folder structuring

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Nav from "@/components/layout/Nav";
+import Footer from "@/components/layout/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Nav />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
